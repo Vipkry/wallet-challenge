4505ffdd3f536b7b93640f49d70be3ab3556517f
vipkry <lolpsxvipkry2@gmail.com>
2017-11-01 13:27:58 -0200

    users created

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wallet-challenge
 API em Rails de um sistema de gerenciamento de cartões de crédito
+
+Ruby version: 2.4.0
+Rails version: 5.1.4
+
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,9 @@
+class UserController < ApplicationController
+  def index
+  	@users = User.all
+  	render json: @users
+  end
+
+  def create
+  end
+end
