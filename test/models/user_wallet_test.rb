45a46b85bb055751b0a60e0ea020e6037ac5a2ee
vipkry <lolpsxvipkry2@gmail.com>
2017-11-02 20:40:29 -0200

    upgraded UserWallet model testing

--- test/models/user_wallet_test.rb
+++ test/models/user_wallet_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserWalletTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid UserWallet" do
+  	wallet = UserWallet.new(:user_id => User.last.id)
+
+  	assert wallet.valid?
+  end
+
+  test "should have user" do
+  	wallet = UserWallet.new()
+	
+	assert_not wallet.valid?
+  end
 end
